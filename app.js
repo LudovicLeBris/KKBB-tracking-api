0e760d282c6b156de37ba0426fbd19c0c92c49e1
LudovicLeBris <ludovic.lebris@gmail.com>
2023-11-03 09:25:25 +0100

    fix[config] some variables for deploying

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
