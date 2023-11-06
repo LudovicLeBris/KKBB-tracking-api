d1e286ff69d52948a937e75dfc935ac9819322bf
LudovicLeBris <ludovic.lebris@gmail.com>
2023-11-06 15:11:13 +0100

    feat[controller] add index for app

--- app.js
+++ app.js
@@ -12,13 +12,18 @@ const baseUrl = 'https://www.kisskissbankbank.com/fr/projects/';
 
 app.use(express.static('public'));
 
-app.get('/:project', async (req, res) => {
-    const url = baseUrl + req.params.project;
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/amount/:project', async (req, res) => {
+    const project = req.params.project;
+    const url = baseUrl + project;
     const scrap = await fetch(url);
     const text = await scrap.text();
     const $ = await cheerio.load(text);
     const amount = await $('div.kiss-Intro__infoBlockLine > div:last > div:first').text();
-    res.render('index', { amount : amount});
+    res.render('amount', { amount : amount, project: project});
 });
 
 app.use(({res}) => {
